dbf1155dd6da31d95ae631ee69dbdaa81e1a16cb
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2025-09-29 10:10:03 +0200

    Initial commit

---
+++ src/test/java/fr/paillaugue/school/r505/R505/R505ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.paillaugue.school.r505.R505;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R505ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
